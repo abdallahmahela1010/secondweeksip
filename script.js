2d140110915cba19a220987673443498c04beac6
abdallahmahela1010 <arakajagoal@gmail.com>
2022-03-07 19:45:57 +0300

    working akan name finder

--- script.js
+++ script.js
@@ -1,36 +1,48 @@
 
+function submitForm(){ 
+    let gender = (document.getElementById)("gender").value;
+    let bday = (document.getElementById)("day").value;
 
+    let dateOfBirth = new Date(bday);  
+    let theDay = dateOfBirth.getDay();
+    // name1.innerText = theDay;
+//   console.log(theDay);
 
-let gender = (document.getElementById)("gender").value;
-let bday = (document.getElementById("day").value);
-let bday1 = "12/10/1996"
+  let mmale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  let ffemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+  
+  if(bday.value===""){
+    alert("Enter Value")
+  }
 
-
-  let dateOfBirth = new Date(bday1);  
-  let theDay = dateOfBirth.getDay(); 
-  document.write("Number of Day: "+theDay);
-  name1.innerText = ("Number:" + theDay) ;
-  console.log(theDay);
-
-
-let mmale = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-let ffemale = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
-
-
-
-function akan(){
-    if(gender === "Male" && theDay === 0  ){
-        name1.innerText = mmale[0];
+  if(gender === "Male" && theDay === 0  ){
+        name1.innerText = "Your Akan name is" + " " + mmale[0];
     }else if(gender === "Male" && theDay === 1){
-        name1.innerText = mmale[1];
+        name1.innerText = "Your Akan name is " + " " + mmale[1];
     }else if(gender === "Male" && theDay === 2){
-        name1.innerText = mmale[2];
+        name1.innerText = "Your Akan name is" + " " + mmale[2];
+    }else if(gender === "Male" && theDay === 3){
+        name1.innerText = "Your Akan name is" + " " + mmale[3];  
+    }else if(gender === "Male" && theDay === 4){
+        name1.innerText = "Your Akan name is" + " " + mmale[4];  
+    }else if(gender === "Male" && theDay === 5){
+        name1.innerText = "Your Akan name is" + " " + mmale[5];  
+    }else if(gender === "Male" && theDay === 6){
+        name1.innerText = "Your Akan name is" + " " + mmale[6];  
+    }else if(gender === "Female" && theDay === 0){
+        name1.innerText = "Your Akan name is" + " " + ffemale[0];  
+    }else if(gender === "Female" && theDay === 1){
+        name1.innerText = "Your Akan name is" + " " + ffemale[1];
+    }else if(gender === "Female" && theDay === 2){
+        name1.innerText = "Your Akan name is" + " " + ffemale[2];
+    }else if(gender === "Female" && theDay === 3){
+        name1.innerText = "Your Akan name is" + " " + ffemale[3];
+    }else if(gender === "Female" && theDay === 4){
+        name1.innerText = "Your Akan name is" + " " + ffemale[4];
+    }else if(gender === "Female" && theDay === 5){
+        name1.innerText = "Your Akan name is" + " " + ffemale[5];
+    }else if(gender === "Female" && theDay === 6){
+        name1.innerText = "Your Akan name is" + " " + ffemale[6];
     }
 }
-
-function submitForm(){
-
-    akan();
-}
-
 
